31210c69b7b8afa82cbd60bcbf14b1fc72b0e6db
atg-coder27 <72323013+atg-coder27@users.noreply.github.com>
2021-03-21 03:07:15 +0530

    Basic Linking done with minor changes

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'LoginPage2.html')
+
+def register(request):
+    return render(request,'Registration form.html')
--- MyProject/urls.py
+++ MyProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.index),
+    path('login/',views.login),
+    path('register/',views.register),
 ]
